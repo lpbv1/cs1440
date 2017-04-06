da2ddd498e1cee74cc504fa6ad9385814fce86b9
lpbv1 <leopb03@gmail.com>
2017-04-05 22:22:34 -0600

    encountering problems (errors in this one)

--- hm6/KeyValue.h
+++ hm6/KeyValue.h
@@ -6,19 +6,19 @@
 template <typename T, typename S>
 class KeyValue {
 private:
-    T m_Key;
-    S m_Value;
-    int m_Id;
+    T* m_Key;
+    S* m_Value;
+    int* m_Id;
 public:
     KeyValue();
     KeyValue(T Key, S Value, int Id);
-    KeyValue(KeyValue &obj);
+    KeyValue(const KeyValue &obj);
     T getKey();
     S getValue();
     int getId();
-    void setKey(T newKey);
-    void setValue(S newValue);
-    void setId(int newId);
+    void setKey(T newKey) const;
+    void setValue(S newValue) const;
+    void setId(int newId) const;
 
 };
 
@@ -29,46 +29,46 @@ KeyValue<T, S>::KeyValue() {
 
 template <typename T, typename S>
 KeyValue<T, S>::KeyValue(T Key, S Value, int Id) {
-    m_Key = Key;
-    m_Value = Value;
-    m_Id = Id;
+    *m_Key = Key;
+    *m_Value = Value;
+    *m_Id = Id;
 }
 
 template <typename T, typename S>
-KeyValue<T, S>::KeyValue(KeyValue &obj) {
-    obj.setKey(this->m_Key);
-    obj.setValue(this->m_Value);
-    obj.setId(this->m_Id);
+KeyValue<T, S>::KeyValue(const KeyValue &obj) {
+    obj.setKey(m_Key);
+    obj.setValue(m_Value);
+    obj.setId(m_Id);
 }
 
 template <typename T, typename S>
 T KeyValue<T, S>::getKey(){
-    return m_Key;
+    return *m_Key;
 }
 
 template <typename T, typename S>
 S KeyValue<T, S>::getValue(){
-    return m_Value;
+    return *m_Value;
 }
 
 template <typename T, typename S>
 int KeyValue<T, S>::getId(){
-    return m_Id;
+    return *m_Id;
 }
 
 template <typename T, typename S>
-void KeyValue<T, S>::setKey(T newKey){
-    m_Key = newKey;
+void KeyValue<T, S>::setKey(T newKey) const{
+    *m_Key = newKey;
 }
 
 template <typename T, typename S>
-void KeyValue<T, S>::setValue(S newValue){
-    m_Value = newValue;
+void KeyValue<T, S>::setValue(S newValue) const{
+    *m_Value = newValue;
 }
 
 template <typename T, typename S>
-void KeyValue<T, S>::setId(int newId){
-    m_Id = newId;
+void KeyValue<T, S>::setId(int newId) const{
+    m_Id = &newId;
 }
 
 
